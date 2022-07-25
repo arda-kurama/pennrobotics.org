7e6d4398122e394b5547b1981c3c2608a839ee2b
ColinMVan <colin_vanhoveln@outlook.com>
2022-07-25 18:08:54 -0400

    changed the landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  smartBackspace: true,
+  typeSpeed: 30,
+  backSpeed: 40,
+  backDelay: 2500,
+  loop: true,
+  loopCount: Infinity,
+});
