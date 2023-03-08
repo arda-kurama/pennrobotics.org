e9a214e1c7dc033cfdb62a73980e67e1c11cd50b
arda-kurama <ardakurama@gmail.com>
2023-03-08 13:35:41 -0500

    removed popup

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
-window.addEventListener("load", function () {
-  setTimeout(function open(event) {
-    document.querySelector(".popup").style.display = "block";
-  }, 2000);
-});
+// window.addEventListener("load", function () {
+//   setTimeout(function open(event) {
+//     document.querySelector(".popup").style.display = "block";
+//   }, 2000);
+// });
 
-document.querySelector("#close").addEventListener("click", function () {
-  document.querySelector(".popup").style.display = "none";
-});
+// document.querySelector("#close").addEventListener("click", function () {
+//   document.querySelector(".popup").style.display = "none";
+// });
