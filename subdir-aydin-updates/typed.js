035f5c55c3f100f78b6330165e90018d8a904c28
arda-kurama <ardakurama@gmail.com>
2024-10-26 22:05:03 -0400

    Removed changes from last commit - did not work properly

--- subdir-aydin-updates/typed.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed("#typed", {
-  stringsElement: "#typed-strings",
-  smartBackspace: true,
-  typeSpeed: 30,
-  backSpeed: 40,
-  backDelay: 2500,
-  loop: true,
-  loopCount: Infinity,
-});
